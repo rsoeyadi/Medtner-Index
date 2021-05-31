1c0db2ff851a8266268f07f2117a0c0cbc064e9a
Ryan Soeyadi <rsoeyadi@me.com>
2021-05-31 11:14:25 -0400

    Fixed movements HTML

--- app.js
+++ app.js
@@ -37,34 +37,38 @@ function receivedData(data) {
         if (piece.op == "TBD" | piece.op == "posthumous") {
             for (var i = 0; i < piece.movements.length; i++) {
                 movements += `<ul class="movements">
-                <li>${JSON.stringify(piece.movements[i]['movementNumber'])}. ${JSON.stringify(piece.movements[i]['movement']).replace(/['"]+/g, '')}</li>
-            </ul>
-        </div>`
+                <li>${JSON.stringify(piece.movements[i]['movementNumber'])}. ${JSON.stringify(piece.movements[i]['movement']).replace(/['"]+/g, '')}</li>`
             }
-              return `<div class="piece">
-                <h1 class="title">${piece.title}</h1>
-                <p class="instrumentation">${piece.instrumentation}</p>
-                <p class="year">${piece.year}</p>
-                <p class="duration">About ${piece.approxDuration} min</p>
-                ${movements}`;
+            
+            return  `<div class="piece">
+                        <h1 class="title">${piece.title}</h1>
+                        <p class="instrumentation">${piece.instrumentation}</p>
+                        <p class="year">${piece.year}</p>
+                        <p class="duration">About ${piece.approxDuration} min</p>
+
+                    <ul class="movements">
+                        ${movements}
+                    </ul>
+                    </div>`;
         }
 
         
         for (var i = 0; i < piece.movements.length; i++) {
-            movements += `<ul class="movements">
-            <li>${JSON.stringify(piece.movements[i]['movementNumber'])}. ${JSON.stringify(piece.movements[i]['movement']).replace(/['"]+/g, '')}</li>
-        </ul>
-    </div>`
+            movements += `
+            <li>${JSON.stringify(piece.movements[i]['movementNumber'])}. ${JSON.stringify(piece.movements[i]['movement']).replace(/['"]+/g, '')}</li>`
         }
-          return `<div class="piece">
-            <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
-            <p class="instrumentation">${piece.instrumentation}</p>
-            <p class="year">${piece.year}</p>
-            <p class="duration">About ${piece.approxDuration} min</p>
-            ${movements}`
 
-            ;
-    })
+          return    `<div class="piece">
+                        <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
+                        <p class="instrumentation">${piece.instrumentation}</p>
+                        <p class="year">${piece.year}</p>
+                        <p class="duration">About ${piece.approxDuration} min</p>
+
+                        <ul class="movements">
+                            ${movements}
+                        </ul>
+                    </div>`;
+            })
     
      pieces = pieces.join("");
      piecesHTML.innerHTML = pieces;
