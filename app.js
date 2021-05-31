ef23cb6463bfac0a47a37656e199174a5865fa60
Ryan Soeyadi <rsoeyadi@me.com>
2021-05-31 15:56:31 -0400

    Text prints when the buttons are clicked

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ var db =  firebase.database(); //create reference to database
 
 var ref = db.ref('compositions'); //target in the database
 
+function targetBtn() {
+var buttons = document.getElementsByTagName('button');
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function (){
+        console.log(this.textContent)
+    }
+}
+}
+
+targetBtn(); //need to figure out how to filter
+
+
 ref.once ('value', receivedData, notReceivedData); //read the data in the db, and callback functions
 
 piecesHTML = document.querySelector('.list-compositions'); //for where we will put our data later
@@ -17,29 +29,14 @@ function receivedData(data) {
 
         var movements = ''; 
 
-        
-        // if (piece.movements.length == 1) {
-        //     for (var i = 0; i < piece.movements.length; i++) {
-        //         movements += `<ul class="movements">
-        //         <li>${JSON.stringify(piece.movements[i]['movement']).replace(/['"]+/g, '')}</li>
-        //     </ul>
-        // </div>`
-        //     }
-        //       return `<div class="piece">
-        //         <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
-        //         <p class="instrumentation">${piece.instrumentation}</p>
-        //         <p class="year">${piece.year}</p>
-        //         <p class="duration">About ${piece.approxDuration} min</p>
-        //         ${movements}`;
-        // }
+       
 
-        //add "no." as in "op. 53 no. 1"
         if (piece.op == "TBD" | piece.op == "posthumous") {
             for (var i = 0; i < piece.movements.length; i++) {
                 movements += `<ul class="movements">
                 <li>${JSON.stringify(piece.movements[i]['movementNumber'])}. ${JSON.stringify(piece.movements[i]['movement']).replace(/['"]+/g, '')}</li>`
             }
-            
+
             return  `<div class="piece">
                         <h1 class="title">${piece.title}</h1>
                         <p class="instrumentation">${piece.instrumentation}</p>
