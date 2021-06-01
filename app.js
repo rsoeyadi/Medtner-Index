c76e16cc04253df4cc97e00ec82006e1f89f0f32
Ryan Soeyadi <rsoeyadi@me.com>
2021-06-01 19:47:18 -0400

    The hard-coded version works but now I need to figure out how to not repeat the closure

--- app.js
+++ app.js
@@ -46,9 +46,41 @@ function receivedData(data) {
             pieces = pieces.join("");
             piecesHTML.innerHTML = pieces;
         }
+
+        function piano_sonata() {
+            
+            let pieces = values.map(function(piece) {
+        
+        
+                for (var i = 0; i < piece.movements.length; i++) {
+                    if (piece.movements[i]['movement'].includes("Piano Sonata")) {
+                        return `<div class="piece">
+                        <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
+                        <p class="instrumentation">${piece.instrumentation}</p>
+                        <p class="year">${piece.year}</p>
+                        <p class="duration">About ${piece.approxDuration} min</p>
+        
+                       
+                    </div>`;
+                    }
+                }
+        
+                if (piece.title.includes("Piano Sonata")) {
+                return    `<div class="piece">
+                                <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
+                                <p class="instrumentation">${piece.instrumentation}</p>
+                                <p class="year">${piece.year}</p>
+                                <p class="duration">About ${piece.approxDuration} min</p>   
+                            </div>`;
+                }
+                    })
+                    pieces = pieces.join("");
+                    piecesHTML.innerHTML = pieces;
+                }
     
       document.getElementById('ara-btn').addEventListener('click', arabesque);
-    
+      document.getElementById('pianoSon-btn').addEventListener('click', piano_sonata);
+
 }
 
 function notReceivedData(error) {
