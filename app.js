e9e19525efaf474454311d710c208b3ece3599be
Ryan Soeyadi <rsoeyadi@me.com>
2021-06-01 19:23:20 -0400

    Closer to figuring out the client-size filtering...

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-function getButtonText(buttonValue) {
-    return buttonValue
-}
+
 
 var db =  firebase.database(); //create reference to database
 
@@ -10,6 +8,7 @@ ref.once ('value', receivedData, notReceivedData); //read the data in the db, an
 
 piecesHTML = document.querySelector('.list-compositions'); //for where we will put our data later
 
+
 function receivedData(data) {
     //we received the data; proceed with parsing it
 
@@ -17,42 +16,28 @@ function receivedData(data) {
 
     let pieces = values.map(function(piece) {
 
-        var movements = ''; 
 
-        // if (piece.op == "TBD" | piece.op == "posthumous") {
-        //     for (var i = 0; i < piece.movements.length; i++) {
-        //         movements += `<ul class="movements">
-        //         <li>${JSON.stringify(piece.movements[i]['movementNumber'])}. ${JSON.stringify(piece.movements[i]['movement']).replace(/['"]+/g, '')}</li>`
-        //     }
-
-        //     return  `<div class="piece">
-        //                 <h1 class="title">${piece.title}</h1>
-        //                 <p class="instrumentation">${piece.instrumentation}</p>
-        //                 <p class="year">${piece.year}</p>
-        //                 <p class="duration">About ${piece.approxDuration} min</p>
-
-        //             <ul class="movements">
-        //                 ${movements}
-        //             </ul>
-        //             </div>`;
-        // }
-
-        
         for (var i = 0; i < piece.movements.length; i++) {
-            movements += `
-            <li>${JSON.stringify(piece.movements[i]['movementNumber'])}. ${JSON.stringify(piece.movements[i]['movement']).replace(/['"]+/g, '')}</li>`
+            if (piece.movements[i]['movement'].includes("Piano Sonata")) {
+                return `<div class="piece">
+                <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
+                <p class="instrumentation">${piece.instrumentation}</p>
+                <p class="year">${piece.year}</p>
+                <p class="duration">About ${piece.approxDuration} min</p>
+
+               
+            </div>`;
+            }
         }
 
-          return    `<div class="piece">
+        if (piece.title.includes("Piano Sonata")) {
+        return    `<div class="piece">
                         <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
                         <p class="instrumentation">${piece.instrumentation}</p>
                         <p class="year">${piece.year}</p>
-                        <p class="duration">About ${piece.approxDuration} min</p>
-
-                        <ul class="movements">
-                            ${movements}
-                        </ul>
+                        <p class="duration">About ${piece.approxDuration} min</p>   
                     </div>`;
+        }
             })
     
      pieces = pieces.join("");
