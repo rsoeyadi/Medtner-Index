b4fabc8fa8f8e48165202625abfbbe696ea88bc2
Ryan Soeyadi <rsoeyadi@me.com>
2021-06-02 15:48:13 -0400

    Still working out filtering system

--- app.js
+++ app.js
@@ -14,21 +14,23 @@ function receivedData(data) {
         text = this.textContent; //we bind the button text to this function and need to use 'this'
 
         let pieces = values.map(function(piece) { //go through each object one by one and create the HTML dynamically
-
-        if (buttonID == "allPiano-btn") {
-            for (var i = 0; i < piece.movements.length; i++) {
-                if (piece.instrumentation === "solo piano") {
-                    console.log(piece.instrumentation);
-                    return  `<div class="piece">
-                            <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
-                            <p class="instrumentation">${piece.instrumentation}</p>
-                            <p class="year">${piece.year}</p>
-                            <p class="duration">About ${piece.approxDuration} min<p>   
-                            </div>`;
-                }
-            }
-        } 
         
+        //'all' buttons
+        if (buttonID == "allPiano-btn" | buttonID == "allViolin-btn" | buttonID == "allVoice-btn") {
+            if (buttonID == "allPiano-btn") {
+                for (var i = 0; i < piece.movements.length; i++) {
+                    if (piece.instrumentation === "solo piano") {
+                        console.log(piece.instrumentation);
+                        return  `<div class="piece">
+                                <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
+                                <p class="instrumentation">${piece.instrumentation}</p>
+                                <p class="year">${piece.year}</p>
+                                <p class="duration">About ${piece.approxDuration} min<p>   
+                                </div>`;
+                    }
+                }
+            } 
+
             else if (buttonID == "allViolin-btn")  {
                 for (var i = 0; i < piece.movements.length; i++) {
                     if (piece.instrumentation === "violin and piano") {
@@ -56,28 +58,31 @@ function receivedData(data) {
                     }
                 }
             } 
+        }
         
-        // if (piece.title.includes(text)) {
-        //     return  `<div class="piece">
-        //             <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
-        //             <p class="instrumentation">${piece.instrumentation}</p>
-        //             <p class="year">${piece.year}</p>
-        //             <p class="duration">About ${piece.approxDuration} min</p>   
-        //             </div>`;
-        // }
+        else {
+            if (piece.title.includes(text)) {
+                
+                return  `<div class="piece">
+                        <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
+                        <p class="instrumentation">${piece.instrumentation}</p>
+                        <p class="year">${piece.year}</p>
+                        <p class="duration">About ${piece.approxDuration} min</p>   
+                        </div>`;
+            }
 
-        // for (var i = 0; i < piece.movements.length; i++) {
-        //     if (piece.movements[i]['movement'].includes(text)) {
-        //         return `<div class="piece">
-        //                 <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
-        //                 <p class="instrumentation">${piece.instrumentation}</p>
-        //                 <p class="year">${piece.year}</p>
-        //                 <p class="duration">About ${piece.approxDuration} min</p>
-        //                 </div>`;
-        //         }
-        //     }
-        
-        })
+            for (var i = 0; i < piece.movements.length; i++) {
+                if (piece.movements[i]['movement'].includes(text)) {
+                    return `<div class="piece">
+                            <h1 class="title">${piece.title}, Op. ${piece.op}</h1>
+                            <p class="instrumentation">${piece.instrumentation}</p>
+                            <p class="year">${piece.year}</p>
+                            <p class="duration">About ${piece.approxDuration} min</p>
+                            </div>`;
+                    }
+                }
+            }
+    })
                 
         pieces = pieces.join(""); //put the HTML all together (get rid of the commas separating the HTML)
         piecesHTML.innerHTML = pieces;
