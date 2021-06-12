1bad3ea33d7bde4a687648ad108df87f8af561d3
Ryan Soeyadi <rsoeyadi@me.com>
2021-06-12 15:58:46 -0400

    Other menus completely disappear when not in focus

--- displayBtns.js
+++ displayBtns.js
@@ -1,19 +1,25 @@
 var genreDivs = document.querySelectorAll('.button-set');
 
 function addHiddenClass() {
+    for (var i = 0; i < genreDivs.length; i++) {
+        genreDivs[i].classList.add('hidden');
+    }
     if (this.classList.contains('hidden')) {
         this.classList.remove('hidden');
     }
 
     else {
         this.classList.add('hidden');
+
     }
 }
 
 window.addEventListener('load', (event) => {
+
     for (i = 0; i < genreDivs.length; i++) {
         let currentDivParent = genreDivs[i].parentElement;
         currentDivParent.style.cursor = 'pointer';
+        
         currentDivParent.onclick = addHiddenClass.bind(genreDivs[i]);
     }
   });
